38ca004af387d10a9ea79f004cab92f4fc57df6f
Arden Zhu <arden.zhu@lafayette148.com>
2020-09-13 19:48:00 -0400

    Add project files.

---
+++ AudioWorkshop.Helpers/WasapiHelper.cs
@@ -0,0 +1,27 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioWorkshop.Helpers
+{
+    public static class WasapiHelper
+    {
+        public static List<MMDevice> GetCaptureDevices()
+        {
+            var deviceEnum = new MMDeviceEnumerator();
+            var devices = deviceEnum.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active).ToList();
+            return devices;
+        }
+
+        public static List<MMDevice> GetRenderDevices()
+        {
+            var deviceEnum = new MMDeviceEnumerator();
+            var devices = deviceEnum.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active).ToList();
+            return devices;
+        }
+
+    }
+}
