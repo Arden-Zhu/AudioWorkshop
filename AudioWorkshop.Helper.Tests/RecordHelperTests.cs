44e0d85363508cc683d94b1706cad559efde4b89
Arden Zhu <arden.zhu@lafayette148.com>
2020-09-13 21:09:58 -0400

    Test record

---
+++ AudioWorkshop.Helper.Tests/RecordHelperTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using AudioWorkshop.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AudioWorkshop.Helper.Tests
+{
+    [TestClass]
+    public class RecordHelperTests
+    {
+        [TestMethod]
+        public void DoRecord()
+        {
+            var devices = DeviceHelper.GetCaptureDevices();
+            var device = devices.Find(m => m.FriendlyName == "Microphone (4- Logitech USB Headset)");
+
+            using (var r = new RecordHelper())
+            {
+                r.Start(device, "test_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".wav");
+                Thread.Sleep(5000);
+                r.Stop();
+            }
+        }
+    }
+}
