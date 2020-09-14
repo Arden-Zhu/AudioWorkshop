54e75d11ee56e2e7e298d7eafaa22023346f2eb7
Arden Zhu <arden.zhu@lafayette148.com>
2020-09-13 22:12:55 -0400

    Record in Winform

--- AudioWorkshop.Recorder/Form1.Designer.cs
+++ AudioWorkshop.Recorder/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(2, 303);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(799, 146);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Location = new System.Drawing.Point(12, 22);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnRecord.TabIndex = 1;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRecord);
+            this.Controls.Add(this.txtOutput);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnRecord;
     }
 }
 
--- AudioWorkshop.Recorder/Form1.cs
+++ AudioWorkshop.Recorder/Form1.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using AudioWorkshop.Helpers;
+using NAudio.CoreAudioApi;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +16,108 @@ namespace AudioWorkshop.Recorder
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        int RecordHotkeyId = 1;
+        MMDevice device;
+        bool isRecording = false;
+        RecordHelper recordHelper;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RegisterHotKeys();
+
+            InitDevices();
+        }
+
+        private void InitDevices()
+        {
+            var devices = DeviceHelper.GetCaptureDevices();
+            this.device = devices.Find(m => m.FriendlyName == "Microphone (4- Logitech USB Headset)");
+
+            this.recordHelper = new RecordHelper();
+        }
+
+        private void RegisterHotKeys()
+        {
+            int HotKeyCode = (int)Keys.F9;
+            Boolean HotKeyRegistered = RegisterHotKey(
+                this.Handle, RecordHotkeyId, 0x0000, HotKeyCode
+            );
+
+            if (!HotKeyRegistered)
+            {
+                Output("Global Hotkey F9 couldn't be registered !");
+            }
+        }
+
+        private void Output(string message)
+        {
+            this.txtOutput.AppendText(message + "\r\n");
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            // Catch when a HotKey is pressed !
+            if (m.Msg == 0x0312)
+            {
+                int id = m.WParam.ToInt32();
+
+                if (id == RecordHotkeyId)
+                {
+                    RecordKeyPressed();
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            RecordKeyPressed();
+        }
+
+        private void RecordKeyPressed()
+        {
+            if (!isRecording)
+            {
+                StartRecording();
+                btnRecord.Text = "Stop";
+                isRecording = true;
+                Output("Start recording.");
+            }
+            else
+            {
+                StopRecording();
+                btnRecord.Text = "Record";
+                isRecording = false;
+                Output("Stop recording.");
+            }
+        }
+
+        private void StopRecording()
+        {
+            this.recordHelper.Stop();
+        }
+
+        private void StartRecording()
+        {
+            this.recordHelper.Start(device, GetFileName());
+        }
+
+        private string GetFileName()
+        {
+            if (!Directory.Exists("Wav"))
+            {
+                Directory.CreateDirectory("Wav");
+            }
+
+            return $"Wav/{DateTime.Now.ToString("yyyyMMdd_HHmmss")}.wav";
         }
     }
 }
